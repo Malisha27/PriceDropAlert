282501c0474b8244fcf9b2d1109eda1501284fc1
Malisha27 <27malisha@gmail.com>
2026-04-19 15:26:05 +0530

    chore: remove unused flashHandler.js

--- static/js/flashHandler.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const popup = document.getElementById("errorPopup");
-  if (popup) {
-    popup.classList.remove("hidden");
-    setTimeout(() => {
-      popup.classList.add("hidden");
-    }, 3000);
-  }
-});
